087a069dc56d39121ad9d7d30c5e1f32c393ea77
xyz <xyzhtml@163.com>
2014-09-04 16:05:15 +0800

    start

---
+++ Gruntfile.js
@@ -0,0 +1,6 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+    });
+    require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+};
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
