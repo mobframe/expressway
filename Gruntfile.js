9aeba20386c56cfe798725da3b30936f760f7f69
xyz <xyzhtml@163.com>
2014-09-26 12:17:05 +0800

    add realtime conf

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
+/**
+ * Grunt configure
+ */
+
 module.exports = function(grunt) {
-    grunt.initConfig({
-    });
     require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
 };
 /* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
